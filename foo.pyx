5b2bd4ec544c7b1e5bcc85eb07ae27251f815b9f
Jean Richard <jean@geemoo.ca>
2012-05-28 10:16:55 -0400

    fixed the segfault issue in the cython.  We have to create a struct, but pass a pointer to it

--- foo.pyx
+++ foo.pyx
@@ -8,18 +8,18 @@ cdef extern from "foo_api.h":
 	int show_int(int a)
 	int show_obj(foo_obj *b)
 
-class FooObj:
+class FooObj(object):
 	def __init__(self):
 		self.a = 0
 		self.b = 0
 
-class Foo:
+class Foo(object):
 	def show_int(self, a):
 		foo.show_int(a)
 
 	def show_obj(self, a):
-		cdef foo_obj *obj
+		cdef foo_obj obj
 		obj.a = a.a
 		obj.b = a.b
-		foo.show_obj(obj)
+		foo.show_obj(&obj)
 	
